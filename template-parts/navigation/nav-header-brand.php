24478d6c77c8988d3daf8f25181266c09c309cce
gribouillenet <webmaster@gribouillenet.fr>
2023-02-08 17:09:13 +0000

    corrections css

--- template-parts/navigation/nav-header-brand.php
+++ template-parts/navigation/nav-header-brand.php
@@ -8,5 +8,5 @@ if (! defined('ABSPATH')) {
     <a class="d-block" href="/" class="navbar-brand" rel="home">
         <span><?php bloginfo('name'); echo ', '; bloginfo('description'); ?></span>
     </a>
-    <button id="menu-toggle" class="menu-toggle-mobile d-flex flex-column align-items-center justify-content-center d-sm-none" aria-controls="primary-menu" aria-expanded="false"><span><?php _e('Menu', 'havre-caravano'); ?></span></button>
+    <button id="menu-toggle" class="menu-toggle-mobile d-flex flex-column align-items-center justify-content-center d-sm-none" aria-controls="primary-menu" aria-expanded="false"><span><?php _e('Menu', 'volant-seminaire'); ?></span></button>
 </div>
