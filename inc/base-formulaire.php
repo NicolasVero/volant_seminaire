976f949e598e0be54a4279e055c6cccc23f26378
gribouillenet <webmaster@gribouillenet.fr>
2023-03-14 17:16:14 +0000

    suite

--- inc/base-formulaire.php
+++ inc/base-formulaire.php
@@ -3,3 +3,7 @@
 <div class="form-content"><label>Lieu du séminaire*</label>[text adresse-activity]</div>
 <div class="form-content"><label>Horaires*</label>de [text time class:walcf7-timepicker] à [text time class:walcf7-timepicker]</div>
 [post_image_checkbox post_image_checkbox-676 posts-number:-1 image-size:medium publish post-type:activites tax-relation:OR value-field:thumbnail orderby:title order:DESC "%title%"]
+
+
+
+le code  wp_redirect( home_url( '/demander-un-devis/' ) ); dans la fonction function add_to_devis(); ne marche pas, corrige-le
