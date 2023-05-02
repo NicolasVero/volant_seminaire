912089321e10304bebc0ed3002d0275bb5ee50e6
gribouillenet <webmaster@gribouillenet.fr>
2023-05-02 12:02:10 +0000

    ajout de plusieurs activités dans l’email

--- inc/activities.php
+++ inc/activities.php
@@ -66,12 +66,9 @@ $args = array(
 
 <div class="devis-form-container">
 	<div class="devis-items-container">
-	
-			
-		<form method="post" action="<?php  //echo esc_url( admin_url('admin-post.php') ); ?>">
-		<!-- <input type="hidden" name="action" value="submit_devis"> -->
-		
+		<form method="post">
 		<?php foreach ( $devis_items_array as $devis_item ) :
+			
 			$activiteID =  get_the_ID();
 			$activite = get_post( $activiteID );
 			$activite_title = esc_html($activite->post_title);
@@ -79,8 +76,7 @@ $args = array(
 			
 			// var_dump($activiteID);
 			$image_url = get_the_post_thumbnail_url( $activiteID, 'medium' );
-			
-			
+
 			?>
 			 
 				<div id="titre_activite-<?= $activiteID ?>" class="devis-item">
@@ -96,27 +92,59 @@ $args = array(
 				
 					<input type="hidden" name="titre_activite-<?= $activiteID ?>" value="<?php echo esc_attr( $activite_title ); ?>">
 					
-					<label for="nombre_personnes">Nombre de personnes :</label>
-					<input value="80" type="number" name="nombre_personnes" required>
+					<label for="nombre_personnes-<?= $activiteID ?>">Nombre de personnes :</label>
+					<input value="80" type="number" name="nombre_personnes-<?= $activiteID ?>" required>
 					
-					<label for="date_activite">Date de l'activité :</label>
-					<input value="2023-10-10" type="date" name="date_activite" required>
+					<label for="date_activite-<?= $activiteID ?>">Date de l'activité :</label>
+					<input value="2023-10-10" type="date" name="date_activite-<?= $activiteID ?>" required>
 					
-					<label for="lieu_seminaire">Lieu du séminaire :</label>
-					<input value="Elbeuf" type="text" name="lieu_seminaire" required>
+					<label for="lieu_seminaire-<?= $activiteID ?>">Lieu du séminaire :</label>
+					<input value="Elbeuf" type="text" name="lieu_seminaire-<?= $activiteID ?>" required>
 					
-					<label for="horaires">Horaires :</label>
-					<input value="9h à 17h" type="text" name="horaires" placeholder="ex: de 9h à 17h" required>
+					<label for="horaires-<?= $activiteID ?>">Horaires :</label>
+					<input value="9h à 17h" type="text" name="horaires-<?= $activiteID ?>" placeholder="ex: de 9h à 17h" required>
 					<button><i class="ti-trash"></i></button>
 				</div>
 			<?php
 			
-			endforeach; 
-			// Utiliser la balise de formulaire pour ajouter $devis_item
-			?> 
+			endforeach;
 			
+			$activitiesID = array('56', '80');
+			
+			$activiteID =  '80';
+			$activite = get_post( $activiteID );
+			$activite_title = esc_html($activite->post_title);
+			$activite_description = esc_html($activite->post_excerpt);
+			
+			?> 
+			<div id="titre_activite-<?= $activiteID  ?>" class="devis-item">
+				<figure class="devis-item-image">
+					<img src="<?= esc_url( $image_url ) ?>" />
+				</figure>
+				<div class="devis-item-content">
+					
+					<h3><?= $activite_title ?></h3>
+					<p><?= $activite_description ?></p>
+					
+				</div>
+			
+				<input type="hidden" name="titre_activite-<?= $activiteID ?>" value="<?php echo esc_attr( $activite_title ); ?>">
 				
+				<label for="nombre_personnes-<?= $activiteID ?>">Nombre de personnes :</label>
+				<input value="22" type="number" name="nombre_personnes-<?= $activiteID ?>" required>
 				
+				<label for="date_activite-<?= $activiteID ?>">Date de l'activité :</label>
+				<input value="2023-05-11" type="date" name="date_activite-<?= $activiteID ?>" required>
+				
+				<label for="lieu_seminaire-<?= $activiteID ?>">Lieu du séminaire :</label>
+				<input value="Thuit de l'Oison" type="text" name="lieu_seminaire-<?= $activiteID ?>" required>
+				
+				<label for="horaires-<?= $activiteID ?>">Horaires :</label>
+				<input value="8h à 16h" type="text" name="horaires-<?= $activiteID ?>" placeholder="ex: de 9h à 17h" required>
+				<button><i class="ti-trash"></i></button>
+			</div>
+			
+						
 				<label for="email">Votre adresse e-mail :</label>
 				<input value="contact@gribouillenet.fr" type="email" name="email" required>
 				
@@ -124,75 +152,88 @@ $args = array(
 			</form>
 		</div>
 		
-</div>
-<?php
-	if ( isset( $_POST['nombre_personnes'] ) && isset( $_POST['date_activite'] ) && isset( $_POST['lieu_seminaire'] ) && isset( $_POST['horaires'] ) )
-		submit_devis($activiteID);	
-?>
-
-	<script>function submit_devis(toto){
-		console.log(toto);
-	}</script>	
-		
-		<?php
-	}
+	</div>
 	
-	add_shortcode( 'devis_form', 'devis_form' );
+	<?php	
+		if ( isset( $_POST['email'] ) ){
+			submit_devis( $activitiesID );
+		}
+}		
+	
+add_shortcode( 'devis_form', 'devis_form' );
 		
 	
-function submit_devis($activiteID) {
-
-			
+function submit_devis( $activitiesID ) {
+			//$activitiesID );
+			foreach( $activitiesID as $activiteID ){
 			// Récupérer les valeurs des champs du formulaire
 			
-			$activite_title = sanitize_text_field( $_POST[ 'titre_activite-' . $activiteID ] );
-			$nombre_personnes = sanitize_text_field( $_POST['nombre_personnes'] );
-			$date_activite = sanitize_text_field( $_POST['date_activite'] );
-			$lieu_seminaire = sanitize_text_field( $_POST['lieu_seminaire'] );
-			$horaires = sanitize_text_field( $_POST['horaires'] );
+			$activite_title 	= htmlspecialchars( sanitize_text_field( $_POST[ 'titre_activite-' . $activiteID ] ) );
+			$nombre_personnes 	= htmlspecialchars( sanitize_text_field( $_POST['nombre_personnes-' . $activiteID ] ) );
+			$date_activite 		= htmlspecialchars( sanitize_text_field( $_POST['date_activite-' . $activiteID ] ) );
+			$lieu_seminaire 	= htmlspecialchars( sanitize_text_field( $_POST['lieu_seminaire-' . $activiteID ] ) );
+			$horaires 			= htmlspecialchars( sanitize_text_field( $_POST['horaires-' . $activiteID ] ) );
 			
-			// Ajouter les données dans la base de données
-			global $wpdb;
+			$all_activities[] = array($activite_title, $nombre_personnes, $date_activite, $lieu_seminaire, $horaires);
 			
-			$table_name = $wpdb->prefix . 'devis';
+			}
 			
-			$data = array(
-				'activite_title' 	=> $activite_title,
-				'activite_id' 		=> $activiteID,
-				'nombre_personnes' 	=> $nombre_personnes,
-				'date_activite' 	=> $date_activite,
-				'lieu_seminaire' 	=> $lieu_seminaire,
-				'horaires' 			=> $horaires,
-			);
-			
-			$format = array(
-				'%s',
-				'%d',
-				'%d',
-				'%s',
-				'%s',
-				'%s',
-			);
-			
-			$wpdb->insert( $table_name, $data, $format );
-			
-			// Envoyer un email à l'adresse personnalisée et à l'adresse de l'internaute qui a rempli le formulaire
-			$to = 'webmaster@gribouillenet.fr'; // Adresse personnalisée
-			$subject = 'Nouvelle demande de devis pour l\'activité ' . get_the_title( $activiteID );
-			$message = 
-						'Titre de l\'activité : ' . $activite_title . "\n" .
-						'Nombre de personnes : ' . $nombre_personnes . "\n" .
-					   'Date de l\'activité : ' . $date_activite . "\n" .
-					   'Lieu du séminaire : ' . $lieu_seminaire . "\n" .
-					   'Horaires : ' . $horaires;
-			$headers = 'From: ' . get_bloginfo( 'name' ) . ' <' . get_option( 'admin_email' ) . '>' . "\r\n";
-			$headers .= 'Reply-To: ' . get_option( 'admin_email' ) . "\r\n";
-			$headers .= 'CC: ' . sanitize_email( $_POST['email'] ) . "\r\n"; // Adresse de l'internaute
-			
-			wp_mail( $to, $subject, $message, $headers );
-			
-			// Rediriger l'utilisateur vers la page de confirmation de demande de devis
-			wp_safe_redirect( home_url( '/confirmation-de-demande-de-devis/' ) );
-			//exit;
+			to_db( $all_activities );
+			send_mail( $all_activities );
 		
-	}
+
+}
+	
+function send_mail( $all_activities ){
+	// Envoyer un email à l'adresse personnalisée et à l'adresse de l'internaute qui a rempli le formulaire
+		$to = 'webmaster@gribouillenet.fr'; // Adresse personnalisée
+		$subject = 'Nouvelle demande de devis pour l\'activité ' . get_the_title( $activiteID );
+		
+		$message = '';
+		
+		for($i = 0; $i < count($all_activities); $i++) {
+		
+			$message .=	'Titre de l\'activité : ' 	. $all_activities[$i][0] . "\n" .
+						'Nombre de personnes : ' 	. $all_activities[$i][1] . "\n" .
+						'Date de l\'activité : ' 	. $all_activities[$i][2] . "\n" .
+						'Lieu du séminaire : ' 		. $all_activities[$i][3] . "\n" .
+						'Horaires : ' 				. $all_activities[$i][4] . "\r\n\n";
+		
+		}
+		$headers = 'From: ' . get_bloginfo( 'name' ) . ' <' . get_option( 'admin_email' ) . '>' . "\r\n";
+		$headers .= 'Reply-To: ' . get_option( 'admin_email' ) . "\r\n";
+		$headers .= 'CC: ' . sanitize_email( $_POST['email'] ) . "\r\n"; // Adresse de l'internaute
+		
+		wp_mail( $to, $subject, $message, $headers );
+		
+		// Rediriger l'utilisateur vers la page de confirmation de demande de devis
+		wp_safe_redirect( home_url( '/confirmation-de-demande-de-devis/' ) );
+		exit;
+}
+
+function to_db( $all_activities ){
+	// Ajouter les données dans la base de données
+	// global $wpdb;
+	// 
+	// $table_name = $wpdb->prefix . 'devis';
+	
+	// $data = array(
+	// 	'activite_title' 	=> $activite_title,
+	// 	'activite_id' 		=> $activiteID,
+	// 	'nombre_personnes' 	=> $nombre_personnes,
+	// 	'date_activite' 	=> $date_activite,
+	// 	'lieu_seminaire' 	=> $lieu_seminaire,
+	// 	'horaires' 			=> $horaires,
+	// );
+	// 
+	// $format = array(
+	// 	'%s',
+	// 	'%d',
+	// 	'%d',
+	// 	'%s',
+	// 	'%s',
+	// 	'%s',
+	// );
+	
+	// $wpdb->insert( $table_name, $data, $format );
+}
