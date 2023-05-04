947f8a02e2a68b886414b82c3ae7739c34968b57
gribouillenet <webmaster@gribouillenet.fr>
2023-05-04 10:01:30 +0000

    mise en place home page

--- front-page.php
+++ front-page.php
@@ -7,12 +7,33 @@ if (! defined('ABSPATH')) {
 get_header();
 
 
-	if(have_posts()) : while(have_posts()) : the_post();
+	if(have_posts()) : 
+		
+		
+		
+	while(have_posts()) : the_post();
+	
+		$id = get_the_ID();
+		$size = 'hero';
+		$hero_image = get_the_post_thumbnail_url($id, $size);
+		$hero_name = get_bloginfo('name');
+		$hero_content = get_the_content();
+	?>
+	
+	<div class="hero-featured">
+		<img src="<?= $hero_image ?>" alt="<? $hero_name ?> accueil">
+		<div class="container">
+			<h1><?= $hero_name ?></h1>
+			<?= $hero_content ?>
+		</div>
+	</div>
 
-	the_content();
+	<?php endwhile; endif;?>
+	
+	<?php if( function_exists('cpt_allActivities') ){
+		cpt_allActivities();
+	}?>
 
-	endwhile; endif;
-
-get_footer();
+<?php get_footer();
 
 ?>
--- inc/images.php
+++ inc/images.php
@@ -5,24 +5,20 @@ function responsive_images($html)
 {
     $html = preg_replace('/(width|height)="\d*"\s/', "", $html);
     return $html;
-}
+}   
 add_filter('post_thumbnail_html', 'responsive_images', 10);
 add_filter('image_send_to_editor', 'responsive_images', 10);
 add_filter('wp_get_attachment_link', 'responsive_images', 10);
 
 //ACTIVATION DES MINIATURES
 add_theme_support('post-thumbnails', array('post'));
-set_post_thumbnail_size(100, 100, true);
+set_post_thumbnail_size(150, 150, true);
 
 //NOUVELLE TAILLE D'IMAGE
 function new_format_image()
 {
-    add_image_size('sticky-news', 680, 475, true);
-    add_image_size('slider', 1200, 840, true);
-    add_image_size('taxonomy', 2540, 1590, true);
-    add_image_size('slider-vehicules', 1730, 980, true);
-    add_image_size('thumbnail-slider-vehicules', 300, 200, true);
-    add_image_size('vignette-vehicules', 475, 275, true);
+    add_image_size('hero', 2540, 530, true);
+    add_image_size('sticker', 248, 206, true);
 }
 add_action('after_setup_theme', 'new_format_image');
 
