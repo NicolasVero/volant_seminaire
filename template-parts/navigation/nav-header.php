e7451ae1cc2acff2c6bd0430a15946baa8253a73
gribouillenet <webmaster@gribouillenet.fr>
2023-05-06 15:23:15 +0000

    mise en place bouton menu

--- template-parts/navigation/nav-header.php
+++ template-parts/navigation/nav-header.php
@@ -11,6 +11,6 @@ include( $urlTemplate . '/inc/datas/data-navigation.php');
 <div id="container-menu-primary" class="container-menu-primary">
 <?php if (has_nav_menu('primary')) { wp_nav_menu($args); } ?>
 
-<button id="menu-toggle" class="menu-toggle-mobile d-flex flex-column align-items-center justify-content-center" aria-controls="primary-menu" aria-expanded="false"><span><?php _e('Menu', 'volant-seminaire'); ?></span></button>
+<button id="menu-toggle" class="menu-toggle d-flex flex-column align-items-center justify-content-center" aria-controls="primary-menu" aria-expanded="false"><span><?php _e('Menu', 'volant-seminaire'); ?></span></button>
 
 </div>
