f7354f2b335e997aa3798669200ce75d08cb838a
gribouillenet <webmaster@gribouillenet.fr>
2023-05-10 12:03:42 +0000

    récupération données erreurs sur formulaire devis

--- inc/functions/function-devis-form.php
+++ inc/functions/function-devis-form.php
@@ -4,46 +4,113 @@ if (! defined('ABSPATH')) {
 	exit;
 }
 
+unset($_SESSION);
 
+// DETECTION ERREURS
 if(isset($_POST['email'])) {
-	echo "coucou";
-	$errors_log = array();
 	
-	if(strlen($_POST['phone']) != 10       ) $errors_log[] = "Le numéro de téléphone renseigné est invalide";
-	if(strlen($_POST['firstname']) < 1     ) $errors_log[] = "Veuillez renseigner votre prénom";
-	if(strlen($_POST['lastname']) < 1      ) $errors_log[] = "Veuillez renseigner votre nom";
-	//if(strlen($_POST['social_reason']) < 1 ) $errors_log[] = "Veuillez renseigner une raison sociale";
-	if(strlen($_POST['message']) < 1       ) $errors_log[] = "Veuillez écrire un message";
+	foreach($_POST as $post) {
+		$post = secure_input($post);
+	}
+		//var_dump($_POST);
+	$errors_log = array();
+	$inputs_errors_name = array();
+	
+	if(strlen($_POST['phone']) < 10 ) {
+		$errors_log[] = "Le numéro de téléphone renseigné est invalide";
+		$inputs_errors_name[] = 'phone';
+	}
+	
+	if(strlen($_POST['firstname']) < 1) {
+		$errors_log[] = "Veuillez renseigner votre prénom";		
+		$inputs_errors_name[] = 'firstname';
+	}
+	
+	if(strlen($_POST['lastname']) < 1 ) {
+		$errors_log[] = "Veuillez renseigner votre nom";
+		$inputs_errors_name[] = 'lastname';
+	}
+	
+	if(strlen($_POST['social_reason']) < 1 ) {
+		$errors_log[] = "Veuillez renseigner une raison sociale";
+		$inputs_errors_name[] = 'social_reason';
+	}
+	
+	if(strlen($_POST['message']) < 1 ) {
+		$errors_log[] = "Veuillez écrire un message";
+		$inputs_errors_name[] = 'message';
+	}
+	
+	
+	
+	if(isset($_POST['hotels']) && isset($_POST['lieu_seminaire_hotel'])) {
+		if($_POST['hotels'] == 'on' && strlen($_POST['lieu_seminaire_hotel']) < 1) {
+			$errors_log[] = "Veuillez sélectionner un hôtel";
+			$inputs_errors_name[] = 'lieu_seminaire_hotel';
+		} 
+	}
+	
+	
 	
 	foreach($_POST as $index => $post) {
 		if(preg_match('/nombre_personnes-/', $index)) {
-			$ids[] = substr(17 - strlen($index));
+			$ids[] = substr($index, 17 - strlen($index));
+		}
+	}
+
+	
+	$inputs = ['nombre_personnes', 'date_activite', 'lieu_seminaire', 'horaires_debut', 'horaires_fin'];
+	$errors = [
+		'Veuillez renseigner un nombre de personne', 
+		'Veuillez renseigner une date pour votre activité', 
+		'Veuillez renseigner un lieu pour votre séminaire', 
+		'Veuillez renseigner un horaire correct', 
+		'Veuillez renseigner un horaire correct'
+	];
+	
+	foreach($ids as $id) {
+		for($i = 0; $i < count($inputs); $i++) {
+			if(strlen($_POST[$inputs[$i] . '-' . $id]) < 1) {
+				$errors_log[] = $errors[$i];
+				$inputs_errors_name[] = $inputs[$i] . '-' . $id;
+			}
 		}
 	}
 	
-	foreach($ids as $id) {
-		if($_POST['nombre-personnes ']) {}
-	}
-	
-	
-	
-	//var_dump($errors_log);
+	session_start();
+		
+	$_SESSION = $_POST;
+	$_SESSION['erreurs'] = array_unique($errors_log);
+	$_SESSION['input_erreurs'] = array_unique($inputs_errors_name);
+	$_SESSION['ids'] = $ids;
 	
 	if(count($errors_log) == 0) {
-		session_start();
-		
-		$_SESSION = $_POST;
-		
-		//var_dump($_SESSION);
-		//header("Location: " . $urlTemplate . "/inc/functions/function-submit-devis.php");
+		header("Location: " . $urlTemplate . "/inc/functions/function-submit-devis.php");
+		echo 'réussi';
 	}
 	
 }
 
 
+function secure_input($input){
+	$input = trim($input);
+	$input = stripslashes($input);
+	$input = htmlspecialchars($input);
+	return $input;
+}
 
 
+// function colorize_errors($inputs_errors) {
+// 	
+// 	foreach($inputs_errors as $input_error) {
+// 		
+// 	}
+// 	
+// }
 
+function rewrite($index) {
+	if(isset($_SESSION[$index])) return $_SESSION[$index];
+}
 
 function get_today_date() {
 	return date('Y-m-d');
@@ -75,20 +142,38 @@ function devis_form() {
 			
 			<!-- Faire appel au fichier de création de formulaire -->
 			
-			<form id="form-devis" method="post" action="<?php echo( $urlTemplate . '/inc/functions/function-submit-devis.php'); ?>">
-			<?php foreach ( $devis_items_array as $devis_item ) :
+			<!-- <form id="form-devis" method="post" action="<?php //echo( $urlTemplate . '/inc/functions/function-submit-devis.php'); ?>"> -->
+			<form id="form-devis" method="post" action="<?php echo('https://volant-seminaire.gribdev.net/demander-un-devis/?activites=activite-mini'); ?>">
+			<?php 
+			
+			//foreach ( $devis_items_array as $devis_item ) :
 				
-				$activiteID =  get_the_ID();
-				$activite = get_post( $activiteID );
-				$activite_title = esc_html($activite->post_title);
-				$activite_description = esc_html($activite->post_excerpt);
-				$activite_image_url = get_the_post_thumbnail_url( $activiteID, 'medium' );
-				$blog_info = get_bloginfo( 'name' );
-				$blog_url = get_bloginfo( 'url' );
-				$blog_admin = get_option( 'admin_email' ); 
+				$activiteID 			= get_the_ID();
+				$activite 				= get_post( $activiteID );
+				$activite_title 		= esc_html($activite->post_title);
+				$activite_description 	= esc_html($activite->post_excerpt);
+				$activite_slug 		  	= esc_html($activite->post_name);
+				$activite_image_url 	= get_the_post_thumbnail_url( $activiteID, 'medium' );
+				$blog_info          	= get_bloginfo( 'name' );
+				$blog_url 				= get_bloginfo( 'url' );
+				$blog_admin 			= get_option( 'admin_email' ); 
 				
 				?>
 				 
+					<input type="hidden" name="blog_info" value="<?= $blog_info ?>">
+					<input type="hidden" name="blog_url" value="<?= $blog_url ?>">
+					<input type="hidden" name="admin_email" value="<?= $blog_admin ?>">
+					<input type="hidden" name="id_principale" value="<?= $activiteID ?>">
+					<input type="hidden" name="activite_slug" value="<?= $activite_slug ?>">
+				 
+				<?php 
+				
+					// if(isset($_POST['email'])) {
+					// 	$cpt = 0;
+					// 	$activiteID = 
+					// }					
+					
+				?>
 					<div id="titre_activite-<?= $activiteID ?>" class="devis-item">
 						<div class="row">
 							<figure class="devis-item-image col-3 col-md-3">
@@ -101,46 +186,35 @@ function devis_form() {
 							<button class="delete-activity col-1"><i class="ti-trash"></i></button>
 						</div>
 						<div class="row">
-							<input type="hidden" name="blog_info" value="<?= $blog_info ?>">
-							<input type="hidden" name="blog_url" value="<?= $blog_url ?>">
-							<input type="hidden" name="admin_email" value="<?= $blog_admin ?>">
-							
-							<!-- ID ACTIVITE PRINCIPALE -->
-							
-							<input type="hidden" name="id_principale" value="<?= $activiteID ?>">
-							
-							<!--  -->
-							
-							
 							<input type="hidden" name="id_activite-<?= $activiteID ?>" value="<?= $activiteID ?>">
 							<input type="hidden" name="titre_activite-<?= $activiteID ?>" value="<?php echo esc_attr( $activite_title ); ?>">
 							<div class="col-12 col-md-6">
 								<div class="d-flex align-items-center input-people">
 									<label class="col-6 pl-0" for="nombre_personnes-<?= $activiteID ?>">Nombre de personnes</label>
-									<input class="col-6" type="number" name="nombre_personnes-<?= $activiteID ?>" required>
+									<input class="col-6" type="number" name="nombre_personnes-<?= $activiteID ?>" >
 								</div>
 								<div class="d-flex align-items-center input-date">
 									<label class="col-6 pl-0" for="date_activite-<?= $activiteID ?>">Date de l'activité</label>
-									<input class="col-6" type="date" name="date_activite-<?= $activiteID ?>" min="<?= get_today_date() ?>" max="<?= get_max_date(6) ?>" required>
+									<input class="col-6" type="date" name="date_activite-<?= $activiteID ?>" min="<?= get_today_date() ?>" max="<?= get_max_date(6) ?>" >
 								</div>
 							</div>
 							<div class="col-12 col-md-6">
 								<div class="d-flex align-items-center input-place">
 									<label class="col-6 pl-0" for="lieu_seminaire-<?= $activiteID ?>">Lieu du séminaire</label>
-									<input type="text" name="lieu_seminaire-<?= $activiteID ?>" required>
+									<input type="text" name="lieu_seminaire-<?= $activiteID ?>" >
 								</div>
 								<div class="d-flex align-items-center input-hours">
 									<label class="col-6 pl-0 d-flex align-items-center" for="horaires_debut-<?= $activiteID ?>">Horaires</label>
-									<div class="d-flex col-6 align-items-center"><span>de</span><input type="time" name="horaires_debut-<?= $activiteID ?>" required> <span>à</span><input type="time" name="horaires_fin-<?= $activiteID ?>" required>
+									<div class="d-flex col-6 align-items-center"><span>de</span><input type="time" name="horaires_debut-<?= $activiteID ?>" > <span>à</span><input type="time" name="horaires_fin-<?= $activiteID ?>" >
 									</div>
 								</div>
 							</div>
 						</div>
 					</div>
 				<?php
-	
-				endforeach;
-		
+				
+				//endforeach;
+				
 				?> 
 				
 				<button class="add-more-activity"><i id="open-menu" class="ti-plus"></i><span>Ajouter une autre activité</span></button>
@@ -150,7 +224,7 @@ function devis_form() {
 					<p class="d-flex justify-content-center">Nous travaillons depuis plus de 10 ans avec<br/>des prestataires Hôteliers.</p>
 					
 					<label class="d-inline-block" for="hotels">Recevoir une offre prestataire hoteliers</label>
-					<input type="checkbox" id="hotels" name="hotels"> 
+					<input type="checkbox" id="hotels" name="hotels" <?php if(isset($_POST['hotels'])) { if($_POST['hotels'] == 'on'){ echo "checked"; }} ?>> 
 					
 					<label class="d-inline-block" for="lieu_seminaire_hotel">Lieu souhaité du séminaire</label>
 					<input type="text" id="lieu_seminaire_hotel" name="lieu_seminaire_hotel" disabled="true"></input>
@@ -160,43 +234,65 @@ function devis_form() {
 					<h3 class="col-12">Mes coordonnées</h3>
 					<div class="col-12 col-md-6">
 						<label class="d-inline-block" for="firstname">Prénom</label>
-						<input class="d-inline-block" type="text" name="firstname" placeholder="Votre prénom" required>
+						<input class="d-inline-block" type="text" name="firstname" placeholder="Votre prénom" value="<?= rewrite('firstname') ?>">
 						
 						<label class="d-inline-block" for="lastname">Nom</label>
-						<input class="d-inline-block" type="text" name="lastname" placeholder="Votre nom" required>
+						<input class="d-inline-block" type="text" name="lastname" placeholder="Votre nom" value="<?= rewrite('lastname') ?>">
 						
 						<label class="d-inline-block" for="social_reason">Raison sociale</label>
-						<input class="d-inline-block" type="text" name="social_reason" placeholder="Entreprise ou particuliers">
+						<input class="d-inline-block" type="text" name="social_reason" placeholder="Entreprise ou particuliers" value="<?= rewrite('social_reason') ?>">
 					</div>
 					<div class="col-12 col-md-6">	
 						<label class="d-inline-block" for="phone">Téléphone</label>
-						<input class="d-inline-block" type="tel" name="phone" placeholder="+33 00 00 00 00 00" required>
+						<input class="d-inline-block" type="tel" name="phone" placeholder="+33 00 00 00 00 00" value="<?= rewrite('phone') ?>">
 						
 						<label class="d-inline-block" for="email">Adresse e-mail</label>
-						<input class="d-inline-block" type="email" name="email" placeholer="Votre adresse email" required>
+						<input class="d-inline-block" type="email" name="email" placeholer="Votre adresse email" value="<?= rewrite('email') ?>">
 					</div>
 				</div>	
 				<div class="row">
 					<h3 class="col-12">Message</h3>
-					<div class="col-12"><textarea name="message" placeholder="Votre message"></textarea></div>
+					<div class="col-12"><textarea name="message" placeholder="Votre message"><?= rewrite('message') ?></textarea></div>
 				</div>
 				<div class="row">
 					<div class="col-12 d-flex justify-content-center">
 						<input type="submit" value="Envoyer">
 					</div>
 				</div>
-	
-					
+
+				
+				<div class="errors" style="background-color: #FFB0B0;">
+					<?php 
+				
+				//var_dump($_POST);
+				//colorize_errors($_SESSION['input_erreurs']);
+				//if(isset($_SESSION['ids']))
+				//	var_dump($_SESSION['ids']);
+
+				if(isset($_SESSION['input_erreurs']))
+					var_dump($_SESSION['input_erreurs']);
+
+					if(isset($_SESSION['erreurs'])) {
+						foreach($_SESSION['erreurs'] as $error) {
+							echo '<p>' . $error . '</p>';		
+						}
+					}
+
+				
+				?>
+				</div>
+				
+
 			</form>
 		</div>
 		
 	</div>
 			
 		<?php
-			cpt_allActivities();
+			 cpt_allActivities();
 			
-			if ( isset( $_POST['email'] ) ){
-				submit_devis( $activitiesID );
-			}
+// 			if ( isset( $_POST['email'] ) ){
+// 				submit_devis( $activitiesID );
+// 			}
 }		
 add_shortcode( 'devis_form', 'devis_form' );
--- inc/functions/function-submit-devis.php
+++ inc/functions/function-submit-devis.php
@@ -1,11 +1,11 @@
 <?php
 $urlTemplate = $_POST['blog_url'];
-
+​
 	if(isset($_POST['email']) ){
 		session_start();
 		
 		$_SESSION = $_POST;
-		var_dump($_SESSION);
+		//var_dump($_SESSION);
 		
 		$activites_rewrite = array();
 		$activites_rewrite_id = array();
@@ -27,8 +27,8 @@ $urlTemplate = $_POST['blog_url'];
 			$activites_rewrite[] = $datas;
 		}
 		
-		var_dump($activites_rewrite);
-		var_dump($activites_rewrite_id);
+		//var_dump($activites_rewrite);
+		//var_dump($activites_rewrite_id);
 		
 		
 		$all_activities = array();
@@ -55,67 +55,67 @@ $urlTemplate = $_POST['blog_url'];
 		// Envoyer un email à l'adresse personnalisée et à l'adresse de l'internaute qui a rempli le formulaire
 		//$to = $_POST['admin_email']; // Adresse personnalisée --> webmaster@gribouillenet.fr
 		$to = 'testappligrib@gmail.com';
-
-
+​
+​
 		if(count($all_activities) == 1 )
 			$subject = 'Demande de devis pour l\'activité ' . $all_activities[0][0] ;
 		else 
 			$subject = 'Demande de devis pour plusieurs activités';
 			
 		$reference = create_reference($_SESSION);
-
+​
 		$message_user   = get_message_user ($all_activities, $_SESSION, $urlTemplate , $reference);
 		$message_admin  = get_message_admin ($all_activities, $_SESSION, $urlTemplate, $reference);
-
+​
 		// echo $message_user;
 		// echo $message_admin;
-
+​
 		$headers = 'From: ' . $_SESSION['blog_info'] . ' <' . $to . '>' . "\r\n";
 		$headers .= 'Reply-To: ' . $to . "\r\n";
 		$headers .= 'CC: ' . htmlspecialchars( $_SESSION['email'] ) . "\r\n"; // Adresse de l'internaute
 		$headers .= "Content-type: text/html; charset=iso-8859-1\r\n";
-
+​
 		//mail( $_POST['email'], $subject, $message_user , $headers );
 		//mail( $to, $subject, $message_admin, $headers );
 	}
-
+​
 	//header('Location:' . $urlTemplate . '/confirmation-demande-de-devis/');
 	
 	function formate_phone_number($numero) {
 		
 		if(strlen($numero) == 10)
 			return implode(' ', str_split($numero, 2));
-
+​
 		if(strlen($numero) > 10)
 			return implode(' ', explode(substr($numero, 2, 1), $numero));
 		
 		return $numero;
 	}
-
+​
 	function formate_date($date) {
 		$date_formate = explode('-', $date);
 		return $date_formate[2] . '.' . $date_formate[1] . '.' . $date_formate[0];
 	}
-
+​
 	function formate_heure($heure) {
 		return str_replace(':', 'h', $heure);
 	}
-
+​
 	function create_reference($user_datas) {
 		$reference = date('ymd');
 		$reference .= strtoupper( substr($user_datas['firstname'], 0, 1) );
 		$reference .= strtoupper( substr($user_datas['lastname'], 0, 1) );
 		$reference .= '_';
-
+​
 		$alphabet="abcdefghijklmnopqrstuvwxyz";
 		$reference .= strtoupper($alphabet[rand(0,25)]);
-
+​
 		return $reference;
 	}
-
+​
 	function get_message_user($all_activities, $user_datas, $image_url, $reference) {
 		$message = get_mail_header($image_url);
-
+​
 		$message .= '
 			<h2 style="margin-left: 10%; margin-top: 50px; font-family: Arial, Helvetica, sans-serif;">Bonjour ' . ucfirst(strtolower($user_datas['firstname'])) . ' ' . ucfirst(strtolower($user_datas['lastname'])) . '</h2>
 			<p style="margin-left: 10%; font-family: Arial, Helvetica, sans-serif;">Référence de votre devis : ' . $reference . '</p>
@@ -125,14 +125,14 @@ $urlTemplate = $_POST['blog_url'];
 						
 		$message .= get_mail_loop($all_activities);
 		$message .= get_mail_footer();
-
+​
 		return $message;
 	}
-
+​
 	function get_message_admin($all_activities, $user_datas, $image_url, $reference) {
-
+​
 		$message = get_mail_header($image_url);
-
+​
 		$message .= '
 			<h2 style="margin-left: 10%; margin-top: 50px; font-family: Arial, Helvetica, sans-serif;">Un utilisateur a fait une demande de devis</h2>
 			<p style="margin-left: 10%; font-family: Arial, Helvetica, sans-serif;">Référence du devis : ' . $reference . '</p>
@@ -145,14 +145,14 @@ $urlTemplate = $_POST['blog_url'];
 			<p style="margin-left: 15%; margin-right: 15%; font-style: italic; margin-bottom: 50px; margin-top: 35px; font-family: Arial, Helvetica, sans-serif;">' . $user_datas['message'] . '</p>
 			
 			<hr>';
-
+​
 		$message .= get_mail_loop($all_activities);
 		$message .= get_mail_footer();
-
+​
 		return $message;
 	}
 	
-
+​
 	function get_mail_header($image_url) {
 		return 
 		'<!DOCTYPE HTML PUBLIC "-//W3C//DTD XHTML 1.0 Transitional //EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
@@ -167,7 +167,7 @@ $urlTemplate = $_POST['blog_url'];
 					<tbody>
 						<tr>
 							<td align="center">
-								<table style="background-color: #333333; height: 75px; min-width: 50%; max-width: 100%;">
+								<table style="background-color: #333333; height: 75px; min-width: 50%; max-width: 1000px;">
 									<tbody>
 										<tr>
 											<td style="text-align: center">
@@ -176,15 +176,15 @@ $urlTemplate = $_POST['blog_url'];
 										</tr> 
 									</tbody>
 								</table>
-								<table style="background-color: white; min-width: 50%; max-width: 50%;">
+								<table style="background-color: white; min-width: 50%; max-width: 1000px;">
 									<tbody>
 										<tr>
 											<td>';
 	}
-
+​
 	function get_mail_loop($all_activities) {
 		$message = '';
-
+​
 		for($i = 0; $i < count($all_activities); $i++) {
 			$message .= '<h3 style="margin-bottom: 5px; margin-left: 10%; font-family: Arial, Helvetica, sans-serif;">' . $all_activities[$i][0] . '</h3>
 			<p style="margin-left: 10%; margin-bottom: 30px; margin-top: 10px; font-family: Arial, Helvetica, sans-serif;">
@@ -192,17 +192,17 @@ $urlTemplate = $_POST['blog_url'];
 			<span style="display: block;">Le ' . formate_date($all_activities[$i][2]) . ', 
 			à ' . ucfirst($all_activities[$i][3]) . ', de ' . formate_heure($all_activities[$i][4]) . ' à ' . formate_heure($all_activities[$i][5]) . '</span></p>';
 		}
-
+​
 		return $message;
 	}
-
+​
 	function get_mail_footer() {
 		return '
 				</td>
 					</tr>
 						</tbody>
 								</table>
-								<table style="background-color: #FF4E00; height: 75px; min-width: 50%; max-width: 100%;">
+								<table style="background-color: #FF4E00; height: 75px; min-width: 50%; max-width: 1000px;">
 									<tbody>
 										<tr style="text-align: center">
 											<td style="color: white; font-family: Arial, Helvetica, sans-serif">© ' . date("Y") . ' Volant Séminaire</td>
@@ -216,4 +216,3 @@ $urlTemplate = $_POST['blog_url'];
 			</body>
 		</html>';
 	}
-	
