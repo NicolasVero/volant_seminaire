0342ccb2385c504d36487d47af87c071f3d7fde2
gribouillenet <webmaster@gribouillenet.fr>
2023-05-10 12:49:35 +0000

    finalisation sorties php fitres activités

--- inc/functions/function-filters.php
+++ inc/functions/function-filters.php
@@ -5,31 +5,52 @@
 			<?php	$terms = get_terms(
 					array(
 						'taxonomy' => 'types_d_activites',
-						array(
-							'hide_empty' => true,
-							'order' => 'DESC'
-						)
-						
+						'hide_empty' => true
 					)
 				);
-	
-				foreach ($terms as $term){
-				$term_id 	= $term -> term_id;
-				$term_slug 	= $term -> slug;
-				$term_name 	= $term -> name;
-				$term_icon = get_field( 'icon_taxo_activite', $term );
-				$term_parent = $term -> parent;
-				var_dump($term);
+
 				
+				$termKey = [];
+				foreach( $terms as $index => $term ){
+					$termKey[] = $index; 
+				}
 				
-						
-					if( $term_parent == 0 ){
-						echo '<li class="item-filter-' .$term_id . '"><a id="toutes" class="filter actif" href="#" id="'. $term_slug . '" class="filter"><img src="' . $term_icon['url'] . '" alt="' . $term_icon['alt'] . '"><span>' . $term_name . '</span></a></li>';
-					}elseif ( $term_parent > 0 ){
-						echo '<li class="item-filter-' .$term_id . '	"><a href="#" id="'. $term_slug . '" class="filter"><img src="' . $term_icon['url'] . '" alt="' . $term_icon['alt'] . '"><span>' . $term_name . '</span></a></li>';
+				$i = 1;
+				for($cpt = 0; $cpt < count($terms); $cpt++) {
+ 	
+					$term_id 	= $terms[$termKey[$cpt]] -> term_id;
+					$term_slug 	= $terms[$termKey[$cpt]] -> slug;
+					$term_name 	= $terms[$termKey[$cpt]] -> name;
+					$term_icon = get_field( 'icon_taxo_activite', $terms[$termKey[$cpt]] );
+					$term_parent = $terms[$termKey[$cpt]] -> parent;
+					
+					if( $term_parent != 0){
+						$sorted_terms[$i] = [
+							'term_id' 		=> $term_id, 
+							'term_slug' 	=> $term_slug, 
+							'term_name'		=> $term_name, 
+							'term_icon'		=> $term_icon, 
+							'term_parent'	=> $term_parent
+							];
+						$i++;
+					}else{
+						$sorted_terms[0] = [
+							'term_id' 		=> $term_id, 
+							'term_slug' 	=> $term_slug, 
+							'term_name'		=> $term_name, 
+							'term_icon'		=> $term_icon, 
+							'term_parent'	=> $term_parent
+						];
+
 					}
-				}			
+				}
 				
+				echo '<li class="item-filter-' . $sorted_terms[0]['term_id'] . '"><a id="toutes" class="filter actif" href="#"><img src="' . $sorted_terms[0]['term_icon']['url'] . '" alt="' . $sorted_terms[0]['term_icon']['alt'] . '"><span>' . $sorted_terms[0]['term_name'] . '</span></a></li>';
+				// 		
+				for($cpt = 1; $cpt < count($sorted_terms); $cpt++){
+					
+					echo '<li class="item-filter-' . $sorted_terms[$cpt]['term_id'] . '	"><a href="#" id="'. $sorted_terms[$cpt]['term_slug'] . '" class="filter"><img src="' . $sorted_terms[$cpt]['term_icon']['url'] . '" alt="' . $sorted_terms[$cpt]['term_icon']['alt'] . '"><span>' . $sorted_terms[$cpt]['term_name'] . '</span></a></li>';
+				}
 				?>
 				
 	</ul>
