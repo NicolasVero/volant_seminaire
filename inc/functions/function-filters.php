87cdac0470d9a56eb22aa17f7c43ce40ece96e91
gribouillenet <webmaster@gribouillenet.fr>
2023-05-10 12:04:59 +0000

    récupération image filtre activités

--- inc/functions/function-filters.php
+++ inc/functions/function-filters.php
@@ -1,22 +1,33 @@
 <div class="container-filtres">	
 
 			<ul class="list-items-filters d-flex justify-content-center">
-			<li class="item-filter"><a id="tous" class="actif" href="#">Toutes</a></li>
 	
 			<?php	$terms = get_terms(
 					array(
 						'taxonomy' => 'types_d_activites',
-						'hide_empty' => true
-				
+						array(
+							'hide_empty' => true,
+							'order' => 'DESC'
+						)
+						
 					)
 				);
 	
 				foreach ($terms as $term){
-				$term_slug = $term -> slug;
-				$term_name = $term -> name;
-		
-				echo '<li class="item-filter"><a href="#" id="'. $term_slug . '" class="filter">' . $term_name . '</a></li>';
+				$term_id 	= $term -> term_id;
+				$term_slug 	= $term -> slug;
+				$term_name 	= $term -> name;
+				$term_icon = get_field( 'icon_taxo_activite', $term );
+				$term_parent = $term -> parent;
+				var_dump($term);
 				
+				
+						
+					if( $term_parent == 0 ){
+						echo '<li class="item-filter-' .$term_id . '"><a id="toutes" class="filter actif" href="#" id="'. $term_slug . '" class="filter"><img src="' . $term_icon['url'] . '" alt="' . $term_icon['alt'] . '"><span>' . $term_name . '</span></a></li>';
+					}elseif ( $term_parent > 0 ){
+						echo '<li class="item-filter-' .$term_id . '	"><a href="#" id="'. $term_slug . '" class="filter"><img src="' . $term_icon['url'] . '" alt="' . $term_icon['alt'] . '"><span>' . $term_name . '</span></a></li>';
+					}
 				}			
 				
 				?>
